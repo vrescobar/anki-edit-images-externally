4c4f026543a21c36425ed090f3cb70f503bf54e2
Carl Severson <carlseverson@gmail.com>
2016-12-30 22:55:33 -0700

    Update editimages.py

--- editimages.py
+++ editimages.py
@@ -4,7 +4,7 @@
 # This is basically a clone of Dimitry Mikheev's "Edit Audio Images" add on (https://ankiweb.net/shared/info/1075177705) with a few work arounds to get it working in Mac OSx.
 # There is no shortcut key to open the file, you must click a button in the editor.
 # If there is any issues with it, please let me know and I will try my best to fix them. That being said, I have never used python before and I might not be able to fix any bugs!
-# https://github.com/carlseverson/anki-edit-images-external ly
+# https://github.com/carlseverson/anki-edit-images-externally
 
 # • Edit Audio Images
 # https://ankiweb.net/shared/info/1040866511
